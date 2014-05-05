6e5207d54db5d40b5f11659116dab543ed01656d
Gustavo <gustavo.bujano@hotmail.com>
2014-05-05 18:31:37 -0500

    Se creo una nueva tabla

---
+++ Base de datos/Benvenutti.05-mayo-2014.sql
@@ -0,0 +1,259 @@
+CREATE DATABASE  IF NOT EXISTS `benvenutti` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `benvenutti`;
+-- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: benvenutti
+-- ------------------------------------------------------
+-- Server version	5.5.37-0ubuntu0.13.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Cliente`
+--
+
+DROP TABLE IF EXISTS `Cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Cliente` (
+  `Id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `Id_persona` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Fecha` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Direccion` varchar(145) COLLATE utf8_spanish_ci NOT NULL,
+  `Id_perfil` int(11) NOT NULL,
+  PRIMARY KEY (`Id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cliente`
+--
+
+LOCK TABLES `Cliente` WRITE;
+/*!40000 ALTER TABLE `Cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Inventario`
+--
+
+DROP TABLE IF EXISTS `Inventario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Inventario` (
+  `Id_producto` int(11) NOT NULL,
+  `Codigo` varchar(30) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `Nombre` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Marca` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Modelo` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `Medida` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Costo_compra` float DEFAULT NULL,
+  `Costo_venta` float DEFAULT NULL,
+  `Imagen` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `Descripcion` varchar(450) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `Tipo` int(11) NOT NULL,
+  `Activo` int(11) NOT NULL,
+  `Fecha` date NOT NULL,
+  `Hora` time NOT NULL,
+  PRIMARY KEY (`Id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Codigo, es para en caso de que sea una cotizaciòn, Medida ya';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Inventario`
+--
+
+LOCK TABLES `Inventario` WRITE;
+/*!40000 ALTER TABLE `Inventario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Inventario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Log`
+--
+
+DROP TABLE IF EXISTS `Log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Log` (
+  `Id_log` int(11) NOT NULL,
+  `Accion` varchar(450) COLLATE utf8_spanish_ci NOT NULL,
+  `Identificador` int(11) NOT NULL,
+  `Query` varchar(450) COLLATE utf8_spanish_ci NOT NULL,
+  `Fecha` date NOT NULL,
+  `Hora` time NOT NULL,
+  PRIMARY KEY (`Id_log`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Log`
+--
+
+LOCK TABLES `Log` WRITE;
+/*!40000 ALTER TABLE `Log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Perfil`
+--
+
+DROP TABLE IF EXISTS `Perfil`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Perfil` (
+  `Id_perfil` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Activo` int(11) NOT NULL,
+  `Fecha` date NOT NULL,
+  PRIMARY KEY (`Id_perfil`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Seleccionar el perfil para asignar al usuario.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Perfil`
+--
+
+LOCK TABLES `Perfil` WRITE;
+/*!40000 ALTER TABLE `Perfil` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Perfil` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Persona`
+--
+
+DROP TABLE IF EXISTS `Persona`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Persona` (
+  `Id_persona` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `Apellidop` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `Apellidom` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `Telefono` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `Cel` varchar(20) COLLATE utf8_spanish_ci DEFAULT '0',
+  `Fecha` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `correo` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `Id_usuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id_persona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='\n';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Persona`
+--
+
+LOCK TABLES `Persona` WRITE;
+/*!40000 ALTER TABLE `Persona` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Persona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `carritodeventa`
+--
+
+DROP TABLE IF EXISTS `carritodeventa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `carritodeventa` (
+  `Id_carrito_de_compra` int(11) NOT NULL AUTO_INCREMENT,
+  `Id_producto` int(11) NOT NULL,
+  `pedido` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `Fecha` date NOT NULL,
+  `Hora` time NOT NULL,
+  PRIMARY KEY (`Id_carrito_de_compra`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='aqui se ira guardando los carritos de venta que se vallan ge';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carritodeventa`
+--
+
+LOCK TABLES `carritodeventa` WRITE;
+/*!40000 ALTER TABLE `carritodeventa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `carritodeventa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `Id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `Usuario` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `Contrasena` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `Fecha` date NOT NULL,
+  `Activo` int(11) NOT NULL,
+  `Id_perfil` int(11) NOT NULL,
+  PRIMARY KEY (`Id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci COMMENT='La tabla usuario tiene 6 columnas, la contrasena va encripta';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'benvenutti','contadora','2014-02-16',0,0),(2,'gustav o','gustavo','2014-02-16',1,3);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `venta`
+--
+
+DROP TABLE IF EXISTS `venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `venta` (
+  `Id_venta` int(11) NOT NULL AUTO_INCREMENT,
+  `Id_cliente` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `IVA` int(11) NOT NULL,
+  `TOTAL` float NOT NULL,
+  `Cancelada` int(11) NOT NULL,
+  `Facturacion` int(11) NOT NULL,
+  `Orden_pedido` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Fecha` date NOT NULL,
+  `Hora` time NOT NULL,
+  PRIMARY KEY (`Id_venta`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Cantidad del producto, el % de IVA, la columnca Cancelada, p';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `venta`
+--
+
+LOCK TABLES `venta` WRITE;
+/*!40000 ALTER TABLE `venta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `venta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-05-05 18:30:27
