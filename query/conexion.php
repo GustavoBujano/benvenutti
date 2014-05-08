e17da08fa62a6cec24ef1d2830db70564ef0bc95
Paco Guevara <pacoguevaraa@gmail.com>
2014-05-07 23:17:14 -0500

    api inventario

--- query/conexion.php
+++ query/conexion.php
@@ -1,6 +1,6 @@
 <?php
-//$db = new mysqli('localhost', 'root', '', 'coleg148_benvenutti');
-$db = new mysqli('10.33.143.26',  'coleg148_ti', 'innovacionesti.2013','coleg148_benvenutti');
+$db = new mysqli('localhost', 'root', 'root', 'benvenutti');
+//$db = new mysqli('10.33.143.26',  'coleg148_ti', 'innovacionesti.2013','coleg148_benvenutti');
 
 if($db->connect_errno > 0)
 {
